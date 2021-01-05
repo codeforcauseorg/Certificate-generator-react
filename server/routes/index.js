959bbd0412b667538659aa7deb4b843f7399bf8d
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-01-05 16:06:04 +0530

    Generate multiple certificates in response to the client on the server

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,6 @@ const fs = require('fs')
 var express = require('express');
 var router = express.Router();
 const { createCanvas, loadImage } = require("canvas");
-const { error } = require('console');
-const { Certificate } = require('crypto');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -12,8 +10,8 @@ router.get('/', function (req, res, next) {
 
 router.post('/cert', function (req, res, next) {
   // console.log(req.body)
-  const certificates = req.body.textProps
-  updateCanvas(certificates).then(buffer => {    
+  // console.log(req.body)
+  updateCanvas(req.body).then(buffer => {    
     res.send(req.body.textProps)
   }).catch(e => {
     res.send(e)
@@ -21,13 +19,20 @@ router.post('/cert', function (req, res, next) {
 })
 
 async function updateCanvas(certificates) {
-  const image = await loadImage('public/templates/t1.png')
+  const {template, textProps, csv} = certificates
+  // console.log(textProps)
+  const image = await loadImage(`public/templates/${template}`)
   var canvas = createCanvas(image.width, image.height)
   ctx = canvas.getContext('2d')
-  certificates.map(async (certificate, index) => {
+  csv.map(async (certificate, index) => {
+    // console.log(certificate)
     ctx.drawImage(image, 0, 0)
-    certificate.map(drawProperties => {
-      const { title, x, y, size } = drawProperties
+    textProps.map((drawProperties, i) => {
+      const { x, y, size } = drawProperties
+      console.log(drawProperties)
+      // console.log("x: " + x + "y: " + y + "size: " + size)
+      const {title} = certificate[i]
+      // console.log(i)
       ctx.fillStyle = "#fff"
       ctx.font = `bold ${size}pt Menlo`
       ctx.fillText(title, x, y)
